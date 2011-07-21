baeab72d5dfd80edbc755d00c5500ce70241f3d3
Sachin Agarwal <sachin@posterous.com>
2011-07-20 21:32:46 -0700

    imports controller!

--- db/migrate/20110719212223_create_imports.rb
+++ db/migrate/20110719212223_create_imports.rb
@@ -4,9 +4,20 @@ class CreateImports < ActiveRecord::Migration
       t.string      "email"
       t.string      "password"
 
+      t.integer     "ext_type"
       t.string      "ext_url"
       t.string      "ext_username"
       t.string      "ext_password"
+      t.integer     "ext_blogid"
+      t.string      "ext_endpoint"
+      
+      t.integer     "state"
+      t.integer     "total"
+      t.integer     "successful"
+      t.integer     "failed"
+      t.string      "error"
+      
+      t.string      "secret"
       
       t.timestamps
     end
